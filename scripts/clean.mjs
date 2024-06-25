e7f4b811850dba4aae6522d162a46474896e435c
Victor Martin <vmleon@gmail.com>
2024-06-25 12:36:48 +0200

    improve clean script

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -21,13 +21,20 @@ await $`rm -f ${publicKeyPath}`;
 const certPath = path.join(__dirname, "..", ".certs");
 await $`rm -rf ${certPath}`;
 
-// await $`rm -f deploy/k8s/backend/application.yaml`;
-await $`rm -f deploy/k8s/overlays/prod/kustomization.yaml`;
+const backendApplicationYamlPath = "deploy/k8s/backend/application.yaml";
+await $`rm -f ${backendApplicationYamlPath}`;
+console.log(`Files in ${chalk.green(backendApplicationYamlPath)} deleted`);
+
+const prodKustomizationPath = "deploy/k8s/overlays/prod/kustomization.yaml";
+await $`rm -f ${prodKustomizationPath}`;
+console.log(`Files in ${chalk.green(prodKustomizationPath)} deleted`);
 
 const certsK8sPath = "deploy/k8s/ingress/.certs";
 await $`rm -rf ${certsK8sPath}`;
 console.log(`Files in ${chalk.green(certsK8sPath)} deleted`);
 
-await $`rm -rf ./.artifacts`;
+const artifactsPath = "./.artifacts";
+await $`rm -rf ${artifactsPath}`;
+console.log(`Files in ${chalk.green(artifactsPath)} deleted`);
 
 config.clear();
